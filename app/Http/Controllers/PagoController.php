c1197c83e8e8e985a54ee2d343011805589fa149
asus <etereosum@gmail.com>
2018-11-19 01:25:52 -0500

    cambio en pagos

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -134,50 +134,10 @@ class PagoController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    public function show($id){}
+    public function edit($id){}
+    public function update(Request $request, $id){}
+    public function destroy($id){}
 
     public function hay_creditos($doc){
         $query = 
