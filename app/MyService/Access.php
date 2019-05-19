8d1cbd141215753ee12da40e7055ead1dd379b3f
pablo adrian <etereosum@gmail.com>
2019-05-19 16:59:06 -0500

    Se prepara este commit para hacer pull desde el repositorio

---
+++ app/MyService/Access.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\MyService;
+use Auth;
+
+class Access
+{
+    public static function in($roles)
+    {
+        $bandera = 1;
+
+        foreach ($roles as $rol) {
+            if (Auth::user()->rol == $rol) {
+                $bandera = 0;
+            }
+        }
+
+        return $bandera;
+    }
+
+    public static function out()
+    {
+        flash()->error('No puede acceder');
+        return back();
+    }
+
+    public static function outJson()
+    {
+        return response()->json([
+            'error'     => true,
+            'data'      => '',
+            'message'   => 'No tiene permisos',
+            'status'    => 400
+        ]);
+    } 
+}
