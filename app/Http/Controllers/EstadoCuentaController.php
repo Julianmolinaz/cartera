f504cbf60d0104c2d8e52c8b2773e2cf611ad5d3
Pablo Gonzalez <pgccrewing@gmail.com>
2019-06-28 12:44:25 -0500

    Se organiza el reporte estado de cuenta, queda faltando imprimir

--- app/Http/Controllers/EstadoCuentaController.php
+++ app/Http/Controllers/EstadoCuentaController.php
@@ -13,227 +13,294 @@ use DB;
 
 class EstadoCuentaController extends Controller
 {
-    public $report;
     public $struct;
     public $credito;
-    public $sanciones;
 
     public function __construct()
     {
         $this->middleware('auth');
-
-        $this->struct = [
-            'tipo_doc'      => '',
-            'num_doc'       => '',
-            'nombre'        => '',
-            'fecha_creacion'=> '',
-            'referencia'    => '',
-            'periodo'       => '',
-            'vlr_cuota'     => '',
-            'num_cts'       => '',
-            'dias_de_pago'  => '',
-            'vlr_credito'   => '',
-            'saldo_total'   => '',
-            'pagos'         => [],
-            'total_abonos$'       => 0,
-            'total_sanciones$'    => 0,
-            'total_sancionesNo'   => 0,
-            'total_prejuridico$'  => 0,
-            'total_juridico$'     => 0,
-            'total_facturas' => 0
-        ];
     }
 
     public function getEstadoCuenta($credito_id)
     {
+        // cargar credito
         $this->credito = Credito::find($credito_id);
 
-        // se cargan los datos generales de la obligación
-        $this->setInfoGeneral();
-        $this->struct['vlr_credito'] = $this->credito->valor_credito;
-        
-        // se cargan todos los pagos realizados hasta la fecha
-        $this->getPagos();
+        // cargar datos
 
-        //se traen las sanciones de la obligacion
-        $this->sanciones = DB::table('sanciones')->where('credito_id',$credito_id)->get();
+        $this->cargarDatosGenerales();
 
-        //inicialización del saldo total
-        $this->struct['saldo_total'] = $this->credito->valor_credito;
-        
-        $this->getSaldos();
+        //generar historial
+
+        $this->cargarHistorico();
+        $now = Carbon::now();
+
+        //dd($this->struct);
 
         return view('admin.estado_de_cuenta.index')
-            ->with('cuenta', $this->struct);
+            ->with('data', $this->struct)
+            ->with('now',$now->toDateString());
     }
 
-    public function getPagos()
+    public function cargarDatosGenerales()
+    {
+        // generar estructura general
+        $this->getStruct();
+
+        //cargar datos generales
+        $this->setDatosGenerales();
+
+    }
+
+    public function getStruct()
+    {
+        $this->struct = [
+            'cliente' => [
+                'nombre'    => '',
+                'doc_type'  => '',
+                'num_doc'   => '',
+            ],
+            'credito' => [
+                'id'            => '',
+                'periodo'       => '',
+                'vlr_cta'       => '',
+                'num_cts'       => '',
+                'dias_pago'     => '',
+                'vlr_credito'   => '',
+                'fecha_apertura'=> ''
+            ],
+            'items'     => [],
+            'totales' => [
+                'total_cuota'           => 0,
+                'total_cuota_parcial'   => 0,
+                'total_num_sanciones'   => 0,
+                'total_sanciones'       => 0,
+                'total_prejuridico'     => 0,
+                'total_juridico'        => 0,
+                'total_saldo'           => 0
+            ]
+        ];
+    }
+
+    public function setDatosGenerales()
+    {
+        $this->struct['cliente']['nombre']      = $this->credito->precredito->cliente->nombre;
+        $this->struct['cliente']['doc_type']    = $this->credito->precredito->cliente->tipo_doc;
+        $this->struct['cliente']['num_doc']     = $this->credito->precredito->cliente->num_doc;
+        $this->struct['credito']['id']          = $this->credito->id;
+        $this->struct['credito']['periodo']     = $this->credito->precredito->periodo;
+        $this->struct['credito']['vlr_cta']     = $this->credito->precredito->vlr_cuota;
+        $this->struct['credito']['num_cts']     = $this->credito->precredito->cuotas;
+        $this->struct['credito']['dias_pago']   = $this->credito->precredito->p_fecha;
+        $this->struct['credito']['vlr_credito'] = $this->credito->valor_credito;
+        $this->struct['credito']['fecha_apertura'] = $this->credito->created_at;
+    }
+
+    public function cargarHistorico()
+    {
+        // generar estructura por cada factura
+        $this->cargarFacturas();
+
+        // definir rangos y cargar cobros
+        $this->cargarCobros();
+
+        // generar totales
+        $this->cargarTotales();
+
+    }
+
+    public function cargarFacturas()
     {
         $facturas = $this->credito->facturas;
-        $array_facturas = [];
 
-        foreach( $facturas as $factura) {
-           $pagos = $this->getAbonosPorConcepto($factura);
-  
-            $temp = [
-                'fecha'         => $factura->fecha,
-                'num_fact'      => $factura->num_fact,
-                'factura_total' => $factura->total,
-                'abonos$'       => $pagos['abono_cuota'],
-                'sanciones$'    => $pagos['abono_sanciones'],
-                'sancionesNo'   => $pagos['num_sanciones'],
-                'prejuridico$'  => $pagos['abono_prejuridico'],
-                'juridico$'     => $pagos['abono_juridico'],
-                'created_at'    => $factura->created_at,
-                'total_a_pagar' => $total_a_pagar,
-                'saldo'         => $total_a_pagar - $factura->total
-            ];
-            
-            $total_a_pagar = $total_a_pagar - $factura->total;
-            
-            $this->struct['total_facturas'] += $factura->total;
-            
-            array_push($array_facturas, $temp);
-
-        }
-
-        $this->struct['pagos'] = $array_facturas;
-    }
-    
-    public function getSaldos()
-    {
-        $ini = new Carbon($this->credito->created_at);
-        $ini = $ini->startOfDay();
-
-
-        if ( ($this->struct['pagos']) > 0) {
-            
-            $this->struct['pagos'][0]['total_a_pagar'] = $this->credito->valor_credito;
-
-            foreach($this->struct['pagos'] as $pago){
-                $fin = new Carbon($pago['created_at']);
-                $fin = $fin->endOfDay();
-
-                // obtiene las sanciones entre el rango del los pagos
-                $sanciones = $this->getSanciones($ini, $fin);
-                $pre_juridicos = $this->getPrejuridicos($ini, $fin);
-                $ini = $fin;
+        if ($facturas) {
+            foreach ($facturas as $factura) {
+                // FACTURA
+                $item = $this->getStructItem();
+                $item['factura']['fecha'] = $factura->created_at;
+                $item['factura']['num']   = $factura->num_fact;
+                $item['factura']['valor'] = $factura->total;
+                
+                foreach ($factura->pagos as $pago) {
+                    $temp = [
+                        'concepto' => $pago->concepto,
+                        'valor'    => $pago->abono
+                    ];
+                    array_push($item['factura']['pagos'], $temp);
+                }
+                //PAGO
+                array_push($this->struct['items'], $item);
             }
-
-            $fin = Carbon::now()->endOfDay();
-
-            $sanciones = $this->getSanciones($ini, $fin);
-            
         }
     }
 
-    public function getSanciones($ini, $fin)
+    public function getStructItem()
     {
-        $sum_sanciones = 0;
-        $count_sanciones = 0;
+        return [
+            'factura' => [
+                'fecha'       => '',
+                'num'         => '',
+                'valor'       => 0,
+                'pagos'       => []
+            ],
+            'estado' => [
+                'total_pagar'     => 0,
+                'saldo'           => 0,
+                'valor_sanciones' => 0,
+                'num_sanciones'   => 0,
+                'prejuridicos'    => [],
+                'juridicos'       => []
+            ]
+        ];
+    }
 
-        foreach($this->sanciones as $sancion) {
+    public function cargarCobros() 
+    {
+        $ini = new Carbon($this->struct['credito']['fecha_apertura']);
 
-            if( $sancion->estado == 'Debe' || $sancion->estado == 'Ok' ) {
-                $created = new Carbon($sancion->created_at);
+        $fin = null;
+        //echo 'ini'.$ini->toDateString().'--';
 
-                if ($created->gt($ini) && $created->lte($fin)) {
-                    $count_sanciones ++;
-                    $sum_sanciones += $sancion->valor;
+        $i = 0;
+
+        for ($i; $i < count($this->struct['items']); $i++) {
+
+            $fin = new Carbon( $this->struct['items'][$i]['factura']['fecha'] );
+            //  echo 'fin'.$fin->toDateString().'<br>';
+
+            // cargar cobros
+            $item = $this->analizarCobros($ini, $fin);
+            $this->struct['items'][$i]['estado']['num_sanciones'] = $item['num_sanciones']; 
+            $this->struct['items'][$i]['estado']['valor_sanciones'] = $item['valor_sanciones']; 
+            $this->struct['items'][$i]['estado']['prejuridicos'] = $item['prejuridicos']; 
+            $this->struct['items'][$i]['estado']['juridicos'] = $item['juridicos']; 
+
+            $ini = $fin;
+            //echo 'ini'.$ini->toDateString().'--';
+        }
+
+        if ($this->credito->estado == 'Cancelado' || 
+            $this->credito->estado == 'Cancelado por refinanciación'){
+            $ini = null;
+             
+        } else {
+            $fin = Carbon::now();
+            // cargar cobros
+            $item2 = $this->analizarCobros($ini, $fin);
+            $this->struct['items'][$i]['estado']['num_sanciones'] = $item2['num_sanciones']; 
+            $this->struct['items'][$i]['estado']['valor_sanciones'] = $item2['valor_sanciones']; 
+            $this->struct['items'][$i]['estado']['prejuridicos'] = $item2['prejuridicos']; 
+            $this->struct['items'][$i]['estado']['juridicos'] = $item2['juridicos']; 
+        }
+    }
+
+    public function analizarCobros($ini, $fin)
+    {
+        $sanciones = $this->getSanciones($ini, $fin);
+        $extras    = $this->getPrejuridicos($ini, $fin);
+
+        return [
+            'num_sanciones'   => $sanciones['num'],
+            'valor_sanciones' => $sanciones['vlr'],
+            'prejuridicos'    => $extras['prejuridicos'],
+            'juridicos'       => $extras['juridicos']
+        ];
+    }
+
+    public function getSanciones($ini,$fin)
+    {
+        // echo $ini->startOfDay().'****'.$fin->endOfDay().'<bR>';
+        $sanciones =  DB::table('sanciones')
+            ->where('credito_id',$this->credito->id)
+            ->where('estado','<>','Exonerada')
+            ->where('created_at','>=',$ini)
+            ->where('created_at','<',$fin)
+            ->get();
+            
+        $vlr_sanciones = 0;
+        foreach($sanciones as $sancion){
+            $vlr_sanciones += $sancion->valor;
+        }
+
+        return [
+            'num' => count($sanciones),
+            'vlr' => $vlr_sanciones
+        ];
+    }
+
+    public function getPrejuridicos($ini, $fin)
+    {
+        $prejuridicos = DB::table('extras')
+            ->where('credito_id',$this->credito->id)
+            ->where('concepto','Prejuridico')
+            ->where('created_at','>=',$ini)
+            ->where('created_at','<',$fin)
+            ->sum('valor');
+
+        $juridicos = DB::table('extras')
+            ->where('credito_id',$this->credito->id)
+            ->where('concepto','Juridico')
+            ->where('created_at','>=',$ini)
+            ->where('created_at','<',$fin)
+            ->sum('valor');
+        
+        return [
+            'prejuridicos' => $prejuridicos,
+            'juridicos'    => $juridicos
+        ];
+    }
+
+    public function cargarTotales()
+    {
+        try {
+            //code...
+            $total_pagar = $this->credito->valor_credito;
+            $saldo  = 0;
+            $i      = 0;
+            for ($i; $i < count($this->struct['items']); $i++) {
+
+                $total_pagar = (float)$total_pagar +
+                (float)$this->struct['items'][$i]['estado']['valor_sanciones'] +
+                (float)$this->struct['items'][$i]['estado']['prejuridicos'] + 
+                (float)$this->struct['items'][$i]['estado']['juridicos'];
+    
+                if (isset($this->struct['items'][$i]['factura'])) {
+
+                    foreach ($this->struct['items'][$i]['factura']['pagos'] as $pago) {
+                        if ($pago['concepto'] == 'Cuota') {
+                            $this->struct['totales']['total_cuota'] += $pago['valor'];
+
+                        } else if ($pago['concepto'] == 'Cuota Parcial') {
+                            $this->struct['totales']['total_cuota_parcial'] += $pago['valor'];
+
+                        } else if ($pago['concepto'] == 'Mora') {
+                            $this->struct['totales']['total_sanciones'] += $pago['valor'];
+
+                        } else if ($pago['concepto'] == 'Prejuridico') {
+                            $this->struct['totales']['total_prejuridico'] += $pago['valor'];
+
+                        } else if ($pago['concepto'] == 'total_juridico') {
+                            $this->struct['totales']['total_juridico'] += $pago['valor'];
+                        }
+
+                    }
+
+
+                    $this->struct['items'][$i]['estado']['total_pagar'] = $total_pagar;
+                    $saldo = $total_pagar - $this->struct['items'][$i]['factura']['valor'];
+                    $this->struct['items'][$i]['estado']['saldo']= $saldo;
+                    
+                    $total_pagar = $saldo;
+                } else {
+                    $this->struct['items'][$i]['estado']['total_pagar'] = $total_pagar;
+                    $this->struct['items'][$i]['estado']['saldo']= $total_pagar;
                 }
             }
+
+
+
+        } catch (\Exception $e) {
+            dd($e);
         }
-        
-        $this->struct['total_sanciones$']   += $sum_sanciones;
-        $this->struct['total_sancionesNo']  += $count_sanciones;
-    
-        return [
-            'sum_sanciones' => $sum_sanciones,
-            'count_sanciones' => $count_sanciones
-        ];
-
-    }
-
-    public function setInfoGeneral() 
-    {
-        $this->struct['tipo_doc'] = $this->credito->precredito->cliente->tipo_doc;
-        $this->struct['num_doc'] = $this->credito->precredito->cliente->num_doc; 
-        $this->struct['nombre'] = $this->credito->precredito->cliente->nombre;
-        $this->struct['fecha_creacion'] = $this->credito->created_at;
-        $this->struct['referencia'] = $this->credito->id;
-        $this->struct['periodo'] = $this->credito->precredito->periodo;
-        $this->struct['vlr_cuota'] = $this->credito->precredito->vlr_cuota;
-        $this->struct['num_cts']   = $this->credito->precredito->cuotas;
-        $this->struct['dias_de_pago'] =
-            ( $this->credito->precredito->s_fecha) ? 
-              $this->credito->precredito->p_fecha.'-'.$this->credito->precredito->s_fecha 
-            : $this->credito->precredito->p_fecha;        
-    }
-
-    public function getAbonosPorConcepto($factura)
-    {
-        $abono_cuota = 0;
-        $abono_prejuridico = 0;
-        $abono_juridico = 0;
-        $abono_sanciones = 0;
-        $num_sanciones   = 0;
-
-        foreach($factura->pagos as $pago){
-            if ($pago->concepto == 'Cuota' || $pago->concepto == 'Cuota Parcial') {
-                $abono_cuota += $pago->abono;
-                $this->struct['total_abonos$'] += $pago->abono;
-            }
-            else if ($pago->concepto == 'Prejuridico') {
-                $abono_prejuridico += $pago->abono;
-                $this->struct['total_prejuridico$'] += $pago->abono;
-            }
-            else if ($pago->concepto == 'Juridico') {
-                $abono_juridico += $pago->abono;
-                $this->struct['total_juridico$'] += $pago->abono;
-            }
-            else if ($pago->concepto == 'Mora') {
-                $abono_sanciones = $pago->abono;
-                $num_sanciones = $abono_sanciones / Variable::all()[0]->vlr_dia_sancion;
-                $this->struct['total_sancionesNo'] += $num_sanciones;
-                $this->struct['total_sanciones$'] += $pago->abono;
-            }
-        }
-        return [
-            'abono_cuota' => $abono_cuota,
-            'abono_sanciones' =>  $abono_sanciones,
-            'num_sanciones' => $num_sanciones,
-            'abono_prejuridico' => $abono_prejuridico,
-            'abono_juridico' => $abono_juridico
-        ];
-    }
-
-    public function getAbonoSancionesTr($factura)
-    {
-        $abono_sanciones = 0;
-        $num_sanciones   = 0;
-
-        foreach($factura->pagos as $pago){
-            if ($pago->concepto == 'Mora') {
-                $abono_sanciones = $pago->abono;
-            }
-        }
-
-        $num_sanciones = $abono_sanciones / Variable::all()[0]->vlr_dia_sancion;
-
-        return [
-            'abono_sanciones' => $abono_sanciones,
-            'num_sanciones'   => (int)$num_sanciones
-        ];
-    }
-
-
-    public function print_A4($credito_id)
-    {
-        $credito = Credito::find($credito_id);
-        $this->setInfoGeneral($credito);
-
-        $this->struct['vlr_credito'] = $credito->valor_credito;
-        
-        $this->getPagos($credito);
     }
 }
