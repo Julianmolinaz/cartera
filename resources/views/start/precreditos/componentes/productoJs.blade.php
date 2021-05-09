a38b35a91cc72d5535f09542e7509250bd6ce9be
Mauro <mauriciogonzalezsalazar@gmail.com>
2021-05-09 13:03:48 -0500

    resolviendo conflictos

--- resources/views/start/precreditos/componentes/productoJs.blade.php
+++ resources/views/start/precreditos/componentes/productoJs.blade.php
@@ -44,16 +44,9 @@
                 } else if (nombre == 'R.T.M') {
                     console.log('entro por rtm');
                     this.ref_productos[index]._vencimiento_rtm = this.addYear(fecha)
-<<<<<<< HEAD
                 }   
 	    },
 	    addYear(fecha) {
-=======
-                }                          
-               
-            },
-            addYear(fecha) {
->>>>>>> 21353bc1e40059fc0ca7e18f589594e669bf5c95
                 // fecha con año + 1
                 if (fecha) {
                     return moment(fecha).add(1, 'years').format('YYYY-MM-DD');
