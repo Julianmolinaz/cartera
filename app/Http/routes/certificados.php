423589da541c48ca3100d07362c2ff78a79c968c
Pablo <etereosum@gmail.com>
2021-09-23 18:14:55 -0500

    MOdificación de permisos para certificados

--- app/Http/routes/certificados.php
+++ app/Http/routes/certificados.php
@@ -7,7 +7,7 @@ Route::get('start/certificados/paz_y_salvo/{credito_id}/{tipo}',[
 ]);
 
 Route::get('start/certificados/preaviso_centrales/{credito_id}/{tipo}',[
-	// 'middleware' => ['permission:expedir_pazysalvo'],
+        'middleware' => ['permission:expedir_pazysalvo'],
 	'uses'  => 'CertificadoController@preavisoCentrales',
 	'as' => 'certificados.preaviso_centrales'
 ]);
