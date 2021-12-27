561346bce97e1877e09db71bbbbd20ee752343d1
Pablo Adrian Gonzalez <etereosum@gmail.com>
2021-12-27 16:18:07 -0500

    ADD: CRUD PRODUCTOS, se modifica el formulario de creación y edición de productos, agragándole los nuevos campos, se agraga boton de eliminar en index productos

--- resources/views/admin/productos/create.blade.php
+++ resources/views/admin/productos/create.blade.php
@@ -1,58 +1,110 @@
+@extends('templates.main2')
+
 @section('title','crear producto')
 
 @section('contenido')
 
+  <div class="col-md-2 col-sm-2"></div>
 
-<div class="row">
-
-  <div class="col-md-4 col-sm-4 "></div>
-
-  <div class="col-md-4 col-sm-4 col-xs-12">
-
+  <div class="col-md-8 col-sm-8 col-xs-12">
     <div class="panel panel-primary">
       <div class="panel-heading">Crear Producto</div>
       <div class="panel-body">
 
-
+        @include('templates.error')
         @include('flash::message')
 
         <form class="form-horizontal form-label-left" action="{{route('admin.productos.store')}}" method="POST">        
 
-          <!-- NOMBRE *******-->
-          <div class="form-group">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-              <label for="">Nombre *:</label>
-              <input type="text" class="form-control" placeholder="ingrese nombre del producto" id="nombre" name="nombre"  value="{{old('nombre')}}" >
-            </div>  
-          </div>
-          <!-- DESCRIPCION *******-->  
-          <div class="form-group">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-              <label>Descripción :</label>
-              <textarea class="form-control" rows="3" id="descripción" name="descripcion" placeholder='Escriba la descripción del producto' autocomplete="off"  value="{{old('descripcion')}}"></textarea>
+            <div class="col-md-12" style="padding:0;">
+                <!-- NOMBRE -->
+                <div class="form-group">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <label for="">Nombre *:</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="ingrese nombre del producto"
+                            id="nombre"
+                            name="nombre"
+                            value="{{old('nombre')}}"
+                        >
+                    </div>  
+                    <div class="col-md-4">
+                        <label for="estado">Estado *</label>
+                        <select 
+                            name="estado"
+                            id="estado"
+                            class="form-control"
+                        >
+                        @foreach($insumos['estados'] as $estado)
+                            <option
+                                value="{{$estado['code']}}" 
+                                {{$estado['code'] === 1 ? 'selected' : ''}}
+                            >{{$estado['label']}}</option>
+                        @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
-         </div>
 
-         <!-- BOTONES **************-->
-         <center>
-            <a href="{{route('admin.productos.index')}}"><button type="button" class="btn btn-primary">Cancelar</button></a>
-            <button type="submit" class="btn btn-danger">&nbsp;&nbsp;Crear&nbsp;&nbsp;</button>
-         </center>   
+            <div class="col-md-12" style="padding:0;">
+                <div class="form-group">
+                    <div class="col-md-3">
+                        <label style="display: block;">Requiere vehículo</label>
+                        <input 
+                            type="checkbox" 
+                            value="1"
+                            name="con_vehiculo"
+                            style="margin: 10px auto"
+                            {{ old('con_vehiculo') ? 'checked' : '' }}
+                        >
+                    </div>
+                    <div class="col-md-3">
+                        <label style="display: block;">Requiere facturación</label>
+                        <input 
+                            type="checkbox"
+                            value="1"
+                            name="con_invoice"
+                            style="margin: 10px auto"
+                            {{ old('con_invoice') ? 'checked' : '' }}
+                        >
+                    </div>
+                </div>
+            </div>
+            <!-- DESCRIPCION -->  
+            <div class="form-group">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <label>Descripción :</label>
+                    <textarea 
+                        class="form-control"
+                        rows="3"
+                        id="descripción"
+                        name="descripcion"
+                        placeholder='Escriba la descripción del producto' 
+                        autocomplete="off"  
+                        value="{{old('descripcion')}}"
+                    ></textarea>
+                </div>
+            </div>
+
+            <!-- BOTONES -->
+            <center>
+                <a 
+                    href="{{route('admin.productos.index')}}"
+                    class="btn btn-default"
+                >Cancelar</a>
+                <button type="submit" class="btn btn-primary">Crear Producto</button>
+            </center>   
 
 
-        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-      </form>
-
-
-
-
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+        </form>
 
       </div>
     </div>
   </div>      
-  <div class="col-md-4 col-sm-4 "></div>
-<div>
+
 
 
 @endsection
-@include('templates.main2')
--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -1,58 +1,109 @@
+
+@extends('templates.main3')
 @section('title','productos')
 
 @section('contenido')
 
 <div class="row">
 
-  <div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="col-md-12 col-sm-12 col-xs-12">
 
-    <div class="panel panel-default">
+        <div class="panel panel-default">
 
-      <div class="panel-heading">Productos
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="{{route('admin.productos.create')}}">
-          <button type="button" class="btn btn-default">Crear producto</button></a>
+        <div class="panel-heading">
+            <h3 style="display: inline;">Productos</h3>
+            <a 
+                href="{{route('admin.productos.create')}}" 
+                class="btn btn-default"
+                style="float:right;"
+            >
+                Crear producto
+            </a>
         </div>
         <div class="panel-body">
-          <p>
-           @include('flash::message')
-           <!--DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction function: <code>$().DataTable();</code>-->
-         </p>
+            <p>
+                @include('flash::message')
+            </p>
 
 
-         <table id="datatable" data-order='[[ 0, "desc" ]]' class="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>    #           </th>
-              <th>    Nombre      </th>
-              <th>    Descripción </th>
-              <th>    Acción     </th>
-            </tr>
-            <tbody>
-
-              @foreach($productos as $producto)
-              <tr>
-                <td> {{$producto->id}}          </td>
-                <td> {{$producto->nombre}}      </td>
-                <td> {{$producto->descripcion}} </td>
-
-                <td> 
-                  <a href="{{route('admin.productos.edit',$producto->id)}}" class = 'btn btn-default btn-xs'>
-                    <span class = "glyphicon glyphicon-pencil" title="Editar"></a>
-
-                    </td>
-                  </tr>   
-
-                  @endforeach
+            <table
+                id="table-productos"
+                data-order='[[ 0, "desc" ]]'
+                class="table table-striped table-bordered"
+                width="100%"
+            >
+                <thead>
+                    <tr>
+                        <th>    updated_at  </th>
+                        <th>    Nombre      </th>
+                        <th>    Estado      </th>
+                        <th>    Descripción </th>
+                        <th>    Requiere vehículo</th>
+                        <th>    Requiere factura</th>
+                        <th>    Acción      </th>
+                    </tr>
                 </thead>
+                <tbody></tbody>
               </table>       
             </div>
           </div>   
         </div>
       </div>
+    @section("js")
 
+    <script>
+        $("#table-productos").DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{url('/api/productos/list')}}",
+            order: [[0, 'desc']],
+            columns: [
+                { data: 'id' },
+                { data: 'nombre' },
+                {
+                    render: (data, type, row) => {
+                        let result = row.estado ? "Activo" : "Inactivo";
+                        return result;
+                    }
+                },
+                {
+                    render: (data, type, row) => {
+                        let result = row.descripcion.substr(0, 30) + " ...";
+                        return result;
+                    }
+                },
+                {
+                    render: (data, type, row) => {
+                        let result = (row.con_vehiculo) ? "Si" : "No";
+                        return result;
+                    }
+                },
+                {
+                    render: (data, type, row) => {
+                        let result = (row.con_invoice) ? "Si" : "No";
+                        return result;
+                    }
+                },
+                {
+                    render: (data, type, row) => {
+                        let routeEdit = "/admin/productos/edit/" + row.id ;
+                        let routeDelete = "/admin/productos/destroy/" + row.id;
+                        let html = `
+                            <a href="${routeEdit}" class='btn btn-default btn-xs'>
+                                <span class = "glyphicon glyphicon-pencil" title="Editar">
+                            <a/>
+                            <a href="${routeDelete}" class='btn btn-default btn-xs'>
+                                <span class = "glyphicon glyphicon-trash" title="Eliminar">
+                            <a/>
+                        `;
+                        return html;
+            
+                    }
+                }
+                
+            ]
+        })
+    </script>
 
-
-
-      @endsection
-      @include('templates.main2')
+    @endsection
+@endsection
