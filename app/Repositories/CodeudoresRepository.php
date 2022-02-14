8ad1aab8041b4f1bc3e45bdd566397cfe7cf51ac
Pablo Adrian Gonzalez <etereosum@gmail.com>
2022-02-14 09:20:24 -0500

    PRO: Antes de hacer pull

--- app/Repositories/CodeudoresRepository.php
+++ app/Repositories/CodeudoresRepository.php
@@ -10,12 +10,15 @@ class CodeudoresRepository
     {
         $codeudor = DB::table('codeudores')
             ->join('clientes', 'codeudores.id', '=', 'clientes.codeudor_id')
-            ->join('municipios', 'codeudores.municipio_id', '=', 'municipios.id')
+            ->leftJoin('municipios', 'codeudores.municipio_id', '=', 'municipios.id')
+            ->leftJoin('municipios as municipios_', 'codeudores.municipioc_id', '=', 'municipios_.id')
             ->where('clientes.id', $clienteId)
             ->select(
                 'codeudores.*',
                 'municipios.nombre as municipio',
                 'municipios.departamento as departamento',
+                'municipios_.nombre as municipio_',
+                'municipios_.departamento as departamento_',
             )
             ->first();
 
